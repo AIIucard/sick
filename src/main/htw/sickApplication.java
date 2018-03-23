7c2b412cfc5352c6c5eb2fb9369c9b1bb94b8b2a
stefan.riedel <stefan.riedel@GR-SNRL-NB1.int.cideon.de>
2018-03-23 15:16:16 +0100

    Add test sout #metoo

--- src/main/htw/sickApplication.java
+++ src/main/htw/sickApplication.java
@@ -5,5 +5,6 @@ public class sickApplication {
 	public static void main(String[] args) {
 		System.out.println("Test");
                 System.out.println("Stef was here");
+                System.out.println("Stef was here #metoo");
 	}
 }
