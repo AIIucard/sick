1996b061cc5fdca713d327306f384c89f6149263
FabeCraft <fabecraft@googlemail.com>
2018-06-27 11:17:18 +0200

    added REST Web Stuff to Webservice Project

---
+++ sickrobotwebservice/sickrobotwebservice/src/main/htw/Main.java
@@ -0,0 +1,11 @@
+package main.htw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("It works? It works!");
+	}
+
+}
