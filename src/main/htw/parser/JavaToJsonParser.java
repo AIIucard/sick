ec1732b0593191c0e51da9e91dceaf4e66679063
FabeCraft <fabecraft@googlemail.com>
2018-08-05 16:54:30 +0200

    Javadoc JavaToJsonParser

--- src/main/htw/parser/JavaToJsonParser.java
+++ src/main/htw/parser/JavaToJsonParser.java
@@ -6,14 +6,27 @@ import main.htw.xml.Area;
 import main.htw.xml.Coordinate;
 import main.htw.xml.Shape;
 
+/**
+ * Java to Json Parser <br>
+ * Parses a JSON formatted string from a given Java object. Useful to prepare
+ * strings in order to be send to the ZigPos RTLS System. The JavaToJsonParsers
+ * supports the following Java objects:
+ * <li>Area
+ */
 public class JavaToJsonParser {
 
+	/**
+	 * Parses a JSON formatted string from an given Area object. The resulting
+	 * string can be used to interact with the ZigPos RTLS System.
+	 * 
+	 * @param area
+	 *            The area object which will be parsed to a JSON formatted String.
+	 * @return JSON formatted string containing a ZigPos readable Area object.
+	 */
 	public static String getAreaJson(Area area) {
 		String jsonString = "";
 
 		jsonString += "{";
-		// add id
-		// jsonString += "\"id\": " + area.getId() + ",";
 
 		// add inOutHysteresis
 		jsonString += "\"inOutHysteresis\": "
@@ -32,15 +45,8 @@ public class JavaToJsonParser {
 		// add shape.Coordinate
 		jsonString += "\"type\": \"" + shape.getType() + "\",";
 		jsonString += "\"coordinates\": [ [";
-		// int count = shape.getCoordinates().size() - 1;
-		// int tmp = 0;
 		for (Coordinate c : shape.getCoordinates()) {
 			jsonString += "[ " + c.getX() + ", " + c.getY() + " ],";
-			// if (tmp != count) {
-			// jsonString += ",";
-			// }
-			//
-			// tmp++;
 		}
 		Coordinate c = shape.getCoordinates().get(0);
 		jsonString += "[" + c.getX() + ", " + c.getY() + "]";
