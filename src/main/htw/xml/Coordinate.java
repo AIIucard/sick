76556e9302124ccb2b6423abdfc65d9159d14520
Kay <kaybode@gmx.de>
2018-08-06 13:08:36 +0200

    Docu added

--- src/main/htw/xml/Coordinate.java
+++ src/main/htw/xml/Coordinate.java
@@ -4,6 +4,10 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * The Class Coordinate is used to save the Coordinates in xml or
+ * load/administrate the xml as java object.
+ */
 @XmlRootElement(name = "coordinate")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Coordinate {
@@ -11,27 +15,60 @@ public class Coordinate {
 	private Double x;
 	private Double y;
 
+	/**
+	 * Empty default constructor required for xml mapping with javax annotations.
+	 */
 	public Coordinate() {
 		// Default constructor
 	}
 
+	/**
+	 * Instantiates a new coordinate.
+	 *
+	 * @param x
+	 *            the x
+	 * @param y
+	 *            the y
+	 */
 	public Coordinate(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * Gets the x.
+	 *
+	 * @return the x
+	 */
 	public double getX() {
 		return x;
 	}
 
+	/**
+	 * Sets the x.
+	 *
+	 * @param x
+	 *            the new x
+	 */
 	public void setX(double x) {
 		this.x = x;
 	}
 
+	/**
+	 * Gets the y.
+	 *
+	 * @return the y
+	 */
 	public double getY() {
 		return y;
 	}
 
+	/**
+	 * Sets the y.
+	 *
+	 * @param y
+	 *            the new y
+	 */
 	public void setY(double y) {
 		this.y = y;
 	}
--- src/main/htw/xml/Shape.java
+++ src/main/htw/xml/Shape.java
@@ -7,6 +7,10 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * The Class Shape is used to save a Shape/Polygon in xml or load/administrate
+ * the xml as java object.
+ */
 @XmlRootElement(name = "shape")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Shape {
@@ -15,27 +19,60 @@ public class Shape {
 	@XmlElement(name = "coordinates")
 	private List<Coordinate> coordinates = null;
 
+	/**
+	 * Empty default constructor required for xml mapping with javax annotations.
+	 */
 	public Shape() {
 		// Default constructor
 	}
 
+	/**
+	 * Instantiates a new shape.
+	 *
+	 * @param type
+	 *            the type
+	 * @param coordinates
+	 *            the coordinates
+	 */
 	public Shape(String type, List<Coordinate> coordinates) {
 		this.type = type;
 		this.coordinates = coordinates;
 	}
 
+	/**
+	 * Gets the type.
+	 *
+	 * @return the type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Sets the type.
+	 *
+	 * @param type
+	 *            the new type
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * Gets the coordinates.
+	 *
+	 * @return the coordinates
+	 */
 	public List<Coordinate> getCoordinates() {
 		return coordinates;
 	}
 
+	/**
+	 * Sets the coordinates.
+	 *
+	 * @param coordinates
+	 *            the new coordinates
+	 */
 	public void setCoordinates(List<Coordinate> coordinates) {
 		this.coordinates = coordinates;
 	}
