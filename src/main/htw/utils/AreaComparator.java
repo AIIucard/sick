ab481103b6d8d2cdbf3ddf1b77a51525ee043eb3
Kay <kaybode@gmx.de>
2018-08-13 16:43:46 +0200

    Added Java Doc Area Comparator

--- src/main/htw/utils/AreaComparator.java
+++ src/main/htw/utils/AreaComparator.java
@@ -4,8 +4,19 @@ import java.util.Comparator;
 
 import main.htw.xml.Area;
 
+/**
+ * This class is used to sort the areas in order of their their distance to
+ * robot.
+ */
 public class AreaComparator implements Comparator<Area> {
 
+	/*
+	 * @param a1 Area1 a2 Area2
+	 * 
+	 * @return 0 if the distance is equal +1 if a1 <a2 , else -1
+	 * 
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
 	@Override
 	public int compare(Area a1, Area a2) {
 		if (a1.getDistanceToRobot() == a2.getDistanceToRobot()) {
